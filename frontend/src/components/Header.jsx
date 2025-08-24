97eae4428986bb54962822ff0865bfd3e2a105c0
Priyansh <priyansh2001singh@gmail.com>
2025-08-24 17:10:32 +0530

    added motion and tailwind

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,16 +9,52 @@ const Header = () => {
     whileInView={{opacity:1, y:0}}
     viewport={{once:true}}
     >
-      <div className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border hover:scale-105 border-neutral-500 transition-all duration-700'>
+      <motion.div className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border hover:scale-105 border-neutral-500 transition-all duration-700'
+      initial={{opacity:0, y:-20}}
+      animate={{opacity:1, y:0}}
+      viewport={{delay:0.2, duration:0.8}}
+      >
         <p>Transform your words into an art</p>
         <img src={assets.star_icon} alt="" />
         
-      </div>
-      <h1 className='text-4xl max-w-[300px] sm:text-6xl sm:max-w-[590px] mx-auto mt-10 text-center'>Transform your Words into an <span className='text-red-600 font-bold'>Art</span> in seconds</h1> 
-      <p className='text-center max-w-xl mx-auto mt-5'>Unleash boundless creativity with AI – Turn your thoughts into stunning visual art instantly. Just type, and watch the magic unfold.</p>
-      <button className='sm:text-lg text-white bg-pink-700 w -auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full hover:scale-105 transition-all duration-700 cursor-pointer'>Generate Image
+      </motion.div>
+      <motion.h1 className='text-4xl max-w-[300px] sm:text-6xl sm:max-w-[590px] mx-auto mt-10 text-center'
+      initial={{opacity:0}}
+      animate={{opacity:1}}
+      viewport={{delay:0.4, duration:2}}
+      >Transform your Words into an <span className='text-red-600 font-bold'>Art</span> in seconds</motion.h1> 
+      <motion.p className='text-center max-w-xl mx-auto mt-5'
+      initial={{opacity:0, y:20}}
+      animate={{opacity:1, y:0}}
+      viewport={{delay:0.6, duration:0.8}}
+      >Unleash boundless creativity with AI – Turn your thoughts into stunning visual art instantly. Just type, and watch the magic unfold.</motion.p>
+      <motion.button className='sm:text-lg text-white bg-pink-700 w -auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full cursor-pointer'
+      whileHover={{scale:1.05}}
+      whileTap={{scale:0.95}}
+      initial={{opacity:0}}
+      animate={{opacity:1}}
+      transition={{default:{duration:0.5}, opacity:{delay:0.8, duration:1}}}
+      >Generate Image
       <img src={assets.star_group} alt="" className='h-6' />
-      </button>
+      </motion.button>
+      <motion.div
+      className='flex flex-wrap justify-around mt-16 gap-3'
+
+      >
+        {Array(6).fill('').map((item,index)=>(
+          <motion.img
+          whileHover={{scale:1.05,duration:0.1}}
+           className='rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10' src={index%2===0 ? assets.sample_img_2 : assets.sample_img_1} alt="" width={70} />
+        ))}
+      </motion.div>
+      <motion.p 
+      initial={{opacity:0}}
+      animate={{opacity:1}}
+      transition={{delay:1.2, duration:0.8}}
+      className='mt-2 text-neutral-500'
+      >
+        Generated Images are here Lets_See
+      </motion.p>
     </motion.div>
   )
 }
